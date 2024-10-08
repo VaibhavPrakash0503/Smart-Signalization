8f8aaf2823fdc34240f262584b93fdb639ef3d9b
vaibhav <vaibhavprakash549@gmail.com>
2024-10-08 23:38:23 +0530

    "test"

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 
+
 mongoose.connect('mongodb://localhost:27017/TrafficLight')
 var db = mongoose.connection
 db.on('error',()=> console.log("Error in Connecting to Database"))
